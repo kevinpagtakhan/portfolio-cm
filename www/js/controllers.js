eb67ffc1d94b0d904f5a180fe24726c214d451d1
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-12-04 01:26:09 -0800

    Login page functionality w/ test credentials

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,7 +7,27 @@ ProjectsCtrl.$inject = ['$http', '$state'];
 
 function LoginCtrl($http, $state) {
   var vm = this;
-  vm.title = 'Login'
+  vm.title = 'Login';
+  vm.user = {};
+
+  vm.handleLogin = function() {
+    if(!!vm.user.name && !!vm.user.password){
+      vm.login(vm.user);
+    } else {
+      alert('Username/password is missing')
+    }
+  }
+
+  vm.login = function() {
+    // test credentials
+    if(vm.user.name === 'user' && vm.user.password === 'password'){
+      $state.go('projects');
+      vm.user = {};
+    } else {
+      vm.user.password = '';
+      alert('Username/password incorrect');
+    }
+  }
 }
 
 function ProjectsCtrl($http, $state) {
